dbd1804628f8a14c87b201f28da4cd88a3204a6c
Vajk Gergely Klukon <61238533+Korboncnok@users.noreply.github.com>
2024-07-11 17:07:47 +0200

    Update vezetok.php

--- vezetok.php
+++ vezetok.php
@@ -174,7 +174,38 @@
     </section><!-- /Trainers Section -->
 
   </main>
+  <hr>
+    <!-- Counts Section -->
+    <section id="counts" class="section ">
 
+      <div class="container" data-aos="fade-up" data-aos-delay="100">
+          
+          
+
+        <div class="row gy-3">
+          <h2>
+            <center><b>Támogatóink</b></center>
+          </h2>
+          
+
+
+        </div>
+
+      </div>
+      
+          <div class="logo-slider" data-v-4ef8651c="">
+      <div class="logos-slide" data-v-4ef8651c="">
+        <img src="./assets/img/sponsors/sahy.png" data-v-4ef8651c="">
+        <img src="./assets/img/sponsors/ministerstvo.png" data-v-4ef8651c="">
+        <img src="./assets/img/sponsors/nivam.png" data-v-4ef8651c="">
+        <img src="./assets/img/sponsors/bethlen.png" data-v-4ef8651c="">
+        <img src="./assets/img/sponsors/szmcs.png" data-v-4ef8651c="">
+      </div>
+    </div>
+      
+
+    </section><!-- /Counts Section -->
+<hr>
   <?php include 'assets/template/footer.php'; ?>
 
   <!-- Scroll Top -->
