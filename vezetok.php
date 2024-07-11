7b6a50916ed081bf9d967ccbfcbeac78ae8949f1
Vajk Gergely Klukon <61238533+Korboncnok@users.noreply.github.com>
2024-07-11 17:04:34 +0200

    hotfix

--- vezetok.php
+++ vezetok.php
@@ -41,29 +41,7 @@
 </head>
 <body class="trainers-page">
 
-  <header id="header" class="header d-flex align-items-center sticky-top">
-    <div class="container-fluid container-xl position-relative d-flex align-items-center">
 
-      <a href="index.html" class="logo d-flex align-items-center me-auto">
-        <!-- Uncomment the line below if you also wish to use an image logo -->
-        <!-- <img src="assets/img/logo.png" alt=""> -->
-        <h1 class="">Sági cserkész</h1>
-      </a>
-
-      <nav id="navmenu" class="navmenu">
-        <ul>
-          <li><a href="index.html">Főoldal</a></li>
-          <li><a href="vezetok.html" class="active">Vezetőink</a></li>
-          <li><a href="events.html">Beszámolók</a></li>
-          <li><a href="contact.html">Kapcsolat</a></li>
-        </ul>
-        <i class="mobile-nav-toggle d-xl-none bi bi-list"></i>
-      </nav>
-
-      <a class="btn-getstarted" href="courses.html">Támogatás</a>
-
-    </div>
-  </header>
   <main class="main">
 
     <!-- Page Title -->
