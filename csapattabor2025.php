ccb28b7e4ea18efd4c8c0082bb8e3fd95892ceea
Vajk Gergely Klukon <klukonvg@gmail.com>
2025-04-13 21:04:23 +0200

    summer camp 2025 information page

---
+++ csapattabor2025.php
@@ -0,0 +1,150 @@
+<!DOCTYPE html>
+<html lang="hu">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+  <title>27. sz. Szent Imre cserkészcsapat</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <link href="assets/img/favicon.png" rel="icon">
+  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Fonts -->
+  <link href="https://fonts.googleapis.com" rel="preconnect">
+  <link href="https://fonts.gstatic.com" rel="preconnect" crossorigin>
+  <link
+    href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,300;1,400;1,500;1,600;1,700;1,800&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Raleway:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+    rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+  <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+  <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+  <!-- Main CSS File -->
+  <link href="assets/css/main.css" rel="stylesheet">
+
+  <?php include 'assets/template/sponsorStyle.php'; ?>
+
+  <!-- =======================================================
+  * Template Name: Mentor
+  * Template URL: https://bootstrapmade.com/mentor-free-education-bootstrap-theme/
+  * Updated: Apr 25 2024 with Bootstrap v5.3.3
+  * Author: BootstrapMade.com
+  * License: https://bootstrapmade.com/license/
+  ======================================================== -->
+</head>
+
+<body class="index-page">
+
+<?php include 'assets/template/header.php'; ?>
+
+  <main class="main">
+
+   <!-- Page Title -->
+   <div class="page-title" data-aos="fade">
+      <div class="heading">
+        <div class="container">
+          <div class="row d-flex justify-content-center text-center">
+            <div class="col-lg-8">
+              <h1>Csapattábor 2025</h1>
+              <p class="mb-0">Az idei csapattáborunkat 2025. július 4-én kezdjük. Minden információt megtalálhatnak a meghívóban.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div><!-- End Page Title -->
+
+    <!-- Events Section -->
+    <section id="events" class="events section">
+
+      <div class="container" data-aos="fade-up">
+
+        <div class="row">
+          <div class="col-12">
+            <div class="card">
+              <div class="card-body">
+                <div class="embed-responsive embed-responsive-16by9">
+                  <iframe class="embed-responsive-item" src="https://drive.google.com/file/d/17_Zsx4OUeQ2uFX2ZINkM4BeU-NeNINcT/preview" style="width: 100%; min-height: 600px;" allowfullscreen></iframe>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        </div>
+
+      </div>
+
+    </section><!-- /Events Section -->
+    
+<hr>
+    <!-- Counts Section -->
+    <section id="counts" class="section ">
+
+      <div class="container" data-aos="fade-up" data-aos-delay="100">
+          
+          
+
+        <div class="row gy-3">
+          <h2>
+            <center><b>Támogatóink</b></center>
+          </h2>
+          
+
+
+        </div>
+
+      </div>
+      
+          <div class="logo-slider" data-v-4ef8651c="">
+      <div class="logos-slide" data-v-4ef8651c="">
+        <img src="./assets/img/sponsors/sahy.png" data-v-4ef8651c="">
+        <img src="./assets/img/sponsors/ministerstvo.png" data-v-4ef8651c="">
+        <img src="./assets/img/sponsors/nivam.png" data-v-4ef8651c="">
+        <img src="./assets/img/sponsors/bethlen.png" data-v-4ef8651c="">
+        <img src="./assets/img/sponsors/szmcs.png" data-v-4ef8651c="">
+      </div>
+    </div>
+      
+
+    </section><!-- /Counts Section -->
+<hr>
+   
+
+  </main>
+
+  <?php include 'assets/template/footer.php'; ?>
+
+  <!-- Scroll Top -->
+  <a href="#" id="scroll-top" class="scroll-top d-flex align-items-center justify-content-center"><i
+      class="bi bi-arrow-up-short"></i></a>
+
+  <!-- Preloader -->
+  <div id="preloader"></div>
+
+  <!-- Vendor JS Files -->
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+  <script src="assets/vendor/aos/aos.js"></script>
+  <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+  <script src="assets/vendor/purecounter/purecounter_vanilla.js"></script>
+  <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+
+  <!-- Main JS File -->
+  <script src="assets/js/main.js"></script>
+  <script>
+              window.onload = function() {
+            var copy = document.querySelector(".logos-slide").cloneNode(true);
+            document.querySelector(".logo-slider").appendChild(copy);
+        };
+  </script>
+
+</body>
+
+</html>
